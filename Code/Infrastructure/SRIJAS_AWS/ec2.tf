d20b2680528d10464d19b3ccb472d9d8f5a556e5
PvPatel-1001 <pvpatel150398@gmail.com>
2021-11-01 12:05:46 -0400

    hosted frontend and backend on different machines and linked them dynamically

--- Code/Infrastructure/SRIJAS_AWS/ec2.tf
+++ Code/Infrastructure/SRIJAS_AWS/ec2.tf
@@ -1,5 +1,5 @@
-# describing an ec2 instance for web server
-resource "aws_instance" "ec2-webserver" {
+# describing an ec2 instance for web backend server
+resource "aws_instance" "backend-webserver" {
     # using standard ami and instance type defined in parameters
     ami           = var.ami_id
     instance_type = var.instance_type
@@ -14,7 +14,7 @@ resource "aws_instance" "ec2-webserver" {
 
     # Assigning name tag
     tags = {
-        Name = "SRIJAS-web-server"
+        Name = "backend-webserver"
     }
 
     # declaring dependencies
@@ -42,11 +42,59 @@ sudo npm i -g npx
 mkdir project
 cd project
 git clone https://github.com/PvPatel-1001/Recipe_Recommender.git
-cd ../backend
+cd Recipe_Recommender/Code/backend
 npm install
 nohup npx nodemon > /dev/null 2>&1 &
+EOF
+}
+
+
+# describing an ec2 instance for frontend-webserver
+resource "aws_instance" "frontend-webserver" {
+    # using standard ami and instance type defined in parameters
+    ami           = var.ami_id
+    instance_type = var.instance_type
+
+    # using the public subnet
+    subnet_id     = module.vpc.subnet_public_id
+
+    # predefined key for terraform in AWS
+    key_name      = "terraform-key"
+    vpc_security_group_ids = [ aws_security_group.ec2-sg.id ]
+    associate_public_ip_address = true
+
+    # Assigning name tag
+    tags = {
+        Name = "frontend-webserver"
+    }
+
+    # declaring dependencies
+    depends_on = [ module.vpc.vpc_id, module.vpc.igw_id, aws_instance.backend-webserver]
+
+    # shell script to be run after initializing the server
+    /*
+        This shell script will install mysql client on web server and add the default address of rds db instance to it's dbdomain file.
+        It will install standard LAMP server for PHP, MySQL and Apache services.
+        It will install git to clone the repository from the master branch.
+        Also other required PHP dependencies
+        It will then clone the repository in to /home/ubuntu/project directory
+        Create a dynamic parameters.json file for all the dynamically created resources information.
+        And move all the web files to /var/www/html where Apache points.
+        Finally it will run the sql script stored in Code/Database/Schema directory to initialize the database and populate the default data in it. 
+    */
+    user_data = <<EOF
+#!/bin/sh
+sudo apt-get update
+curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+sudo apt-get install --yes nodejs
+sudo apt install npm -y
+sudo npm install -g n
+sudo npm i -g npx
+mkdir project
+cd project
+git clone https://github.com/PvPatel-1001/Recipe_Recommender.git
 cd Recipe_Recommender/Code/frontend
-sudo sed -i 's/##serverIp##/54.87.50.91/g' /src/apis/recipeDB.js
+sudo sed -i 's/##serverIp##/${aws_instance.backend-webserver.public_ip}/g' /project/Recipe_Recommender/Code/frontend/src/apis/recipeDB.js
 npm install
 npm start
 EOF
--- Code/Infrastructure/SRIJAS_AWS/outputs.tf
+++ Code/Infrastructure/SRIJAS_AWS/outputs.tf
@@ -1,18 +1,35 @@
 # defining the outputs needed after completion of execution
 
-# outputs for ec2 webserver
-output "ec2-webserver-dns" {
-    value = aws_instance.ec2-webserver.public_dns
+# outputs for ec2 frontend webserver
+output "frontend-webserver-dns" {
+    value = aws_instance.frontend-webserver.public_dns
 }
 
-output "ec2-webserver-ip" {
-    value = aws_instance.ec2-webserver.public_ip
+output "frontend-webserver-ip" {
+    value = aws_instance.frontend-webserver.public_ip
 }
 
-output "ec2-webserver-private-dns" {
-    value = aws_instance.ec2-webserver.private_dns
+output "frontend-webserver-private-dns" {
+    value = aws_instance.frontend-webserver.private_dns
 }
 
-output "ec2-webserver-private-ip" {
-    value = aws_instance.ec2-webserver.private_ip
+output "frontend-webserver-private-ip" {
+    value = aws_instance.frontend-webserver.private_ip
+}
+
+# outputs for backend webserver
+output "backend-webserver-dns" {
+    value = aws_instance.backend-webserver.public_dns
+}
+
+output "backend-webserver-ip" {
+    value = aws_instance.backend-webserver.public_ip
+}
+
+output "backend-webserver-private-dns" {
+    value = aws_instance.backend-webserver.private_dns
+}
+
+output "backend-webserver-private-ip" {
+    value = aws_instance.backend-webserver.private_ip
 }
