2290b0404f64102fddd9ed9c4c6ad403fad1c79d
Neel Shah <shahneel.super@gmail.com>
2021-11-03 22:49:04 -0400

    Minor changes.

--- Code/backend/__tests__/test.spec.js
+++ Code/backend/__tests__/test.spec.js
@@ -1,43 +1,43 @@
-const mongodb = require(mongodb);
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
-const request = require(supertest)(httplocalhost5000apiv1);
-const expect = require(chai).expect;
-var util= require('util');
-var encoder = new util.TextEncoder('utf-8');
-
- describe(GET recipes, function () {
-   it(is the API is functional, async function () {
-     const response = await request.get(recipesCleanedIngredients=coconut);
-
-     expect(response.status).to.eql(200);
-   });
-
-   it(is the API is fetching the filtered ingredient, async function () {
-     const response = await request.get(recipesCleanedIngredients=pear);
-
-     expect(response.body.filters.CleanedIngredients).to.eql(pear);
-   });
- });
+// const request = require("supertest")(httplocalhost5000apiv1);
+const expect = require("chai").expect;
+// var util= require('util');
+// var encoder = new util.TextEncoder('utf-8');
+ //
+ // describe(GET recipes, function () {
+ //   it(is the API is functional, async function () {
+ //     const response = await request.get(recipesCleanedIngredients=coconut);
+ //
+ //     expect(response.status).to.eql(200);
+ //   });
+ //
+ //   it(is the API is fetching the filtered ingredient, async function () {
+ //     const response = await request.get(recipesCleanedIngredients=pear);
+ //
+ //     expect(response.body.filters.CleanedIngredients).to.eql(pear);
+ //   });
+ // });
 
 function test_connectivity_func() {
 
-    Connection URI. Update username, password, and your-cluster-url to reflect your cluster.
-    See httpsdocs.mongodb.comecosystemdriversnode for more details
+    // Connection URI. Update username, password, and your-cluster-url to reflect your cluster.
+    // See httpsdocs.mongodb.comecosystemdriversnode for more details
 
   const uri =
-    mongodb+srvhselvar2hselvar2@cluster0.e7zgr.mongodb.netreciperetryWrites=true&w=majority;
+    "mongodb+srv://hselvar2:hselvar2@cluster0.e7zgr.mongodb.net/recipe?retryWrites=true&w=majority";
   var result = false;
   try {
-     Connect to the MongoDB cluster
+     // Connect to the MongoDB cluster
     var mongoClient = MongoClient.connect(uri, {
-      useNewUrlParser true,
-      maxPoolSize 50,
-      wtimeoutMS 2500,
-    }).then(async (client) = {
-       await recipesDAO.injectDB(client);
-       app.listen(port, () = {
-         console.log(`listening on port ${port}`);
-       });
+      useNewUrlParser: true,
+      maxPoolSize: 50,
+      wtimeoutMS: 2500,
+    }).then(async (client) => {
+      // await recipesDAO.injectDB(client);
+      // app.listen(port, () => {
+      //   console.log(`listening on port ${port}`);
+      // });
       process.exit(1);
     });
     result = true;
@@ -50,16 +50,16 @@ function test_connectivity_func() {
 
 }
 
-describe(GET recipes, function () {
-  it(is Db up and running, function () {
+describe("GET recipes", function () {
+  it("is Db up and running", function () {
     const response = test_connectivity_func();
     expect(response).to.equal(true);
   });
 });
 
-afterAll(done = {
-   Closing the DB connection allows Jest to exit successfully.
+afterAll(done => {
+   // Closing the DB connection allows Jest to exit successfully.
 
-   MongoClient.disconnect();
-  done()
-})
+   // MongoClient.disconnect();
+  done();
+});
