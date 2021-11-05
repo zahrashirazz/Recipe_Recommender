c3e88693f998c1438d9b029de57232957ded5baf
PvPatel-1001 <PvPatel-1001@users.noreply.github.com>
2021-11-05 03:49:15 +0000

    Prettified Code!

--- Code/backend/dao/recipesDAO.js
+++ Code/backend/dao/recipesDAO.js
@@ -72,7 +72,7 @@ export default class RecipesDAO {
 
       if (flagger == "true") {
         var transporter = nodemailer.createTransport({
-          host: 'smtp.gmail.com',
+          host: "smtp.gmail.com",
           port: 465,
           secure: true,
           auth: {
