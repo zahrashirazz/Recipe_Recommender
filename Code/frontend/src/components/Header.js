09ae15eeb39fc66ebbb2d3ba94f8b3577167c7c2
SimranBosmiya <bosamiyasimran@gmail.com>
2021-11-20 22:03:23 -0500

    navbar

--- Code/frontend/src/components/Header.js
+++ Code/frontend/src/components/Header.js
@@ -1,10 +1,15 @@
 import React from "react";
 
+
 function Header() {
   return (
-    <div id="head_container">
-      <h1 id="head"> Recipe Recommender</h1>
-    </div>
+<ul className='navbar-ul'>
+  <li className='navbar-li nav-brand'>Recipe Recommender</li>
+  <li className='navbar-li navbar-li-right'>Account</li>
+</ul>
+    // <div id="head_container">
+    //   <h1 id="head"> Recipe Recommender</h1>
+    // </div>
   );
 }
 export default Header;
