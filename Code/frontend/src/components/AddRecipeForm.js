229b6d71bd408856f57e6d85bba3956e6afc27d5
thosaniparth <thosaniparth0@gmail.com>
2021-12-04 01:07:47 -0500

    Added recipe time in add new recipes

--- Code/frontend/src/components/AddRecipeForm.js
+++ Code/frontend/src/components/AddRecipeForm.js
@@ -27,6 +27,7 @@ class Form extends Component {
     dict["recipe_instructions"] = document.getElementById(
       "recipe_instructions"
     ).value;
+    dict["recipe_time"]=document.getElementById("recipe_time").value;
     this.props.sendRecipeFormData(dict);
   };
 
@@ -72,7 +73,14 @@ class Form extends Component {
               </div>
             </div>
           </div>
-
+          <div className="row pb-1">
+            <div className="input-group col-lg-4 bg-danger text-white">
+              <label class="sideLabel"> Recipe Time Taken: </label> <br />
+              <div className="input-group-append">
+                <input type="text" id="recipe_time" />
+              </div>
+            </div>
+          </div>
           <div className="row pb-1">
             <div className="input-group col-lg-4 bg-danger text-white">
               <div className="input-group-append">
