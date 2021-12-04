62bb902aaeffd3564fc6c6ea24f091125f0dae09
thosaniparth <thosaniparth0@gmail.com>
2021-12-04 01:23:31 -0500

    Added Recipe URL field for add new recipes

--- Code/frontend/src/components/AddRecipeForm.js
+++ Code/frontend/src/components/AddRecipeForm.js
@@ -28,6 +28,7 @@ class Form extends Component {
       "recipe_instructions"
     ).value;
     dict["recipe_time"]=document.getElementById("recipe_time").value;
+    dict["recipe_url"]=document.getElementById("recipe_url").value;
     this.props.sendRecipeFormData(dict);
   };
 
@@ -78,6 +79,14 @@ class Form extends Component {
               <label class="sideLabel"> Recipe Time Taken: </label> <br />
               <div className="input-group-append">
                 <input type="text" id="recipe_time" />
+              </div>
+            </div>
+          </div>
+          <div className="row pb-1">
+            <div className="input-group col-lg-4 bg-danger text-white">
+              <label class="sideLabel"> Recipe Image URL: </label> <br />
+              <div className="input-group-append">
+                <input type="text" id="recipe_url" />
               </div>
             </div>
           </div>
