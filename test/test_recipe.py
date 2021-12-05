59f624f03b61f1a2b5facaee6d2ad5496e6c7520
Parth Thosani <43791395+thosaniparth@users.noreply.github.com>
2021-12-04 19:25:42 -0500

    Update test_recipe.py

--- test/test_recipe.py
+++ test/test_recipe.py
@@ -1,9 +1,9 @@
 import requests
 
 def test_get_result():
-    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=Tomato")
+    result = requests.get("http://localhost:5000/api/v1/recipes")
     assert result is not None
 
 def test_get_result_status():
-    result = requests.get("http://localhost:5000/api/v1/recipes?CleanedIngredients=Tomato")
+    result = requests.get("http://localhost:5000/api/v1/recipes")
     assert result.status_code == 200
