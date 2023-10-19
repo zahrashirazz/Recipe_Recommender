bbbcae249c99514c58b7224938e6c75a3036a62a
Jay Vishaal J <jayvishaal17065@cse.ssn.edu.in>
2023-10-19 12:47:11 -0400

    Removed potential user data leak and added updated only wanted user methods

--- Code/backend/api/userauth.route.js
+++ Code/backend/api/userauth.route.js
@@ -5,7 +5,5 @@ const userAuthService = require("./userauth.service");
 const router = express.Router();
 
 //URl to get the users
-router.route("/getAllUsers").get(userAuthService.getAllUsers);
 router.route("/authorizeUser").post(userAuthService.findUser);
-router.route("/saveRecipe").put(userAuthService.saveRecipe);
 module.exports = router;
